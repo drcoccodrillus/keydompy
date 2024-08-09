68f39f5309deb41c86b9afb1402d2be56a64affc
drcoccodrillus <dr.coccodrillus@gmail.com>
2024-08-09 14:29:12 +0200

    Change to __init__.py

--- src/keydom/__init__.py
+++ src/keydom/__init__.py
@@ -1,5 +1,23 @@
-# keydom/__init__.py
+__version__ = "0.1"
+__author__ = "dr.coccodrillus"
+__email__ = "dr.coccodrillus@gmail.com"
+__license__ = "Apache License"
+
 
 from .keydom import KeydomManager
 
 __all__ = ['KeydomManager']
+
+DEFAULT_CONFIG = {
+    "keydom_ip": "172.26.20.100",
+    "keydom_port": 443,
+    "keydom_protocol": "https",
+    "keydom_username": "admin",
+    "keydom_password": "admin",
+}
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info("Module loaded")
